e40e4b0480edd0845213a7cd1f7aa1471370d04d
sylsocha <sylwia9socha@gmail.com>
2024-05-28 09:06:05 +0200

    update in progress

--- update.php
+++ update.php
@@ -1,13 +1,21 @@
 <?php
 
 $conn = new mysqli('localhost', 'root', '', 'grafix_database');
-$sql = 'update users set imie=?, nazwisko=?, ulica=?, nr_domu=?, nr_mieszkania=?, miasto=?, kod_pocztowy=?, e_mail=?, nr_tel=? where id_user=1';
+$sql = "update users set imie='$_POST[imie]',
+                         nazwisko='$_POST[nazwisko]',
+                         ulica='$_POST[ulica]',
+                         nr_domu='$_POST[nr_domu]',
+                         nr_mieszkania=$_POST[nr_mieszk],
+                         miasto='$_POST[miasto]',
+                         kod_pocztowy='$_POST[kodpocztowy]',
+                         e_mail='$_POST[email]',
+                         nr_tel=$_POST[nr_tel]
+             where id_user=1";
 
-$prep=$conn->prepare($sql);
+$conn->query($sql);
 
-$prep->bind_param('sssiissss', $_POST['imie'], $_POST['nazwisko'], $_POST['ulica'], $_POST['nr_domu'], $_POST['nr_mieszk'], $_POST['miasto'], $_POST['kodpocztowy'], $_POST['email'], $_POST['nr_tel']);
+/*$sql2 = "";*/
 
-var_dump($_POST);
 echo '<br><hr>';
 echo 'Edytowane rekordy: ' . $conn->affected_rows;
 /*
